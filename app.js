36e57dee44a73c64a79699b416834ea1c6aa7fa0
joce2311 <jnortegamontecino@gmail.com>
2022-08-04 03:11:38 +0000

    terminado

---
+++ app.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+import "bootstrap";
+import "./style.css";
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+
+window.onload = function() {
+  //write your code hereclear
+
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++)
+        console.log(pronoun[i], adj[j], noun[k]);
+    }
+  }
+};
